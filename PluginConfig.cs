3cbd7fb13f9f71000c380fd6cf064b079763ddb3
MSWS <imodmaker@gmail.com>
2024-12-16 00:02:11 -0800

    Add config for dead hiding, alias support, and some optimizations

--- PluginConfig.cs
+++ PluginConfig.cs
@@ -4,8 +4,9 @@ namespace HidePlayers;
 
 public class PluginConfig : IBasePluginConfig
 {
-    public string Command { get; set; } = "css_hidemodels";
+    public string Command { get; set; } = "css_hidemodels,css_hide";
     public string Hidden { get; set; } = "@all";
+    public bool HideDead { get; set; } = true;
 
-    public int Version { get; set; } = 1;
+    public int Version { get; set; } = 2;
 }
